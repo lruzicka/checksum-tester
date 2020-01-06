e79f8227681d37bc1dcdacd337d7d5e49a0d9dd8
Lukáš Růžička <lruzicka@redhat.com>
2020-01-06 13:32:03 +0100

    Add CLI arguments.

--- checksum-tester.py
+++ checksum-tester.py
@@ -5,6 +5,7 @@ Checksum Tester calculates the checksums for the given ISO and compares it the w
 It automatically downloads the ISO image from Koji.
 """
 
+import argparse
 import datetime
 import glob
 import os
@@ -12,6 +13,18 @@ import fedfind.release
 import subprocess
 import wget
 
+def read_cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--release', default="Rawhide", help="Fedora release")
+    parser.add_argument('-c', '--compose', default=None, help="Compose identifier, YYYYMMDD for Rawhide.")
+    parser.add_argument('-a', '--arch', default="x86_64", help="Architecture")
+    parser.add_argument('-v', '--variant', default="Everything", help="Variant (Everything, Server, Workstation, Spins, Cloud")
+    parser.add_argument('-s', '--subvariant', default=None, help="Subvariant (For spins: KDE, LXCD, XFCE)")
+    parser.add_argument('-t', '--type', default=None, help="Type of image (For server: boot, dvd)")
+    parser.add_argument('-p', '--purge', default=False, help="Use 'True' if you want to delete downloaded after testing.")
+    args = parser.parse_args()
+    return args
+    
 def provide_compose(rel="Rawhide", comp=None, arch="x86_64", variant="Everything", subvariant=None, typ=None):
     """ Returns a compose download link base on given criteria. """
     if not comp and rel=="Rawhide":
@@ -45,16 +58,17 @@ def download_iso(composes):
         url = compose['url']
         filename = return_iso_filename(url)
         if filename in os.listdir():
-            print(f"The ISO file {filename} seems to be downloaded already, skipping.")
+            print(f"The ISO file {filename} seems to be downloaded already, skipping the download.")
         else:
             wget.download(url)
             print("")
 
-def purge_images(isofiles):
+def purge_images(files):
     """ Deletes all iso files from the working directory. """
-    for iso in isofiles:
-        print(f"Deleting {iso}")
-        os.remove(iso)
+    for f in files:
+        target = return_iso_filename(f)
+        print(f"Deleting {target}")
+        os.remove(target)
 
 def find_isofiles():
     """ Finds all files with the .iso extension in the working directory. """
@@ -75,14 +89,9 @@ def test_compose_sha256(composes):
             calculated_sha = calculate.stdout.decode('utf-8').split(" ")
             calculated_sha = calculated_sha[0].strip()
         if expected_sha == calculated_sha:
-            print(f"The SHA256 checksum test PASSED for this ISO image.")
-            print(f"Expected and calculated checksum: {expected_sha}")
-            results[url] = "PASSED"
+            results[filename] = "PASSED"
         else:
-            print(f"The SHA256 checksum test FAILED for this ISO image.")
-            print(f"Expected checksum: {expected_sha}")
-            print(f"Calculated checksum: {calculated_sha}")
-            results[url] = "FAILED"
+            results[filename] = "FAILED"
     return results
 
 def test_compose_md5(composes):
@@ -95,26 +104,49 @@ def test_compose_md5(composes):
         output = checkmd.stdout.decode('utf-8')
         print(output)
         if checkmd.returncode == 0:
-            print("The MD5 checksum test PASSED.")
             results[filename] = "PASSED"
         elif checkmd.returncode == 1:
-            print("The MD5 checksum test FAILED.")
             results[filename] = "FAILED"
         elif checkmd.returncode == 2:
-            print("The MD5 checksum test was skipped.")
             results[filename] == "SKIPPED"
         else:
             print(checkmd.stderr.decode('utf-8'))
     return results
 
+def print_results(field, results):
+    """ Prints results of the tests. """
+    print(f"================ {field} RESULTS ============================")
+    print("")
+    for result in results.keys():
+        print(f"{result}: {results[result]}")
+    print("")
+    
+def print_available_composes(composes):
+    """ Prints a list of available ISO files to test. """
+    if len(composes) > 1:
+        message = f"{len(composes)} image files matching the criteria found: "
+    elif len(composes) == 1:
+        message = "One image file matching the criteria found: "
+    else:
+        message = "No image file matching the criteria found."
+    print(message)
 
-composes = provide_compose(comp="20200101", arch="x86_64", variant="Cloud")
-print(f"{len(composes)} comp. matching the criteria found.")
-download_iso(composes)
-test_compose_sha256(composes)
-test_compose_md5(composes)
+def main():
+    """ Main program. """
+    args = read_cli()
+    composes = provide_compose(rel=args.release, comp=args.compose, arch=args.arch, variant=args.variant, subvariant=args.subvariant, typ=args.type)
+    purge = args.purge
+    print_available_composes(composes)
+    download_iso(composes)
+    results = test_compose_sha256(composes)
+    print_results("SHA256 CHECKSUM", results)
+    results = test_compose_md5(composes)
+    print_results("MD5 CHECKSUM", results)
+    if purge == True:
+        purge_images(composes)
 
-
+if __name__ == '__main__':
+    main()
 
 
 
